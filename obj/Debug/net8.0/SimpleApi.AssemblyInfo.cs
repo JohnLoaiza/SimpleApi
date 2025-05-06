d9331b5d8877d66505290fee3492123c66a200e8
John <jhon91811@gmail.com>
2025-05-06 11:40:32 -0500

    adjusment 4

--- obj/Debug/net8.0/SimpleApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be7d2515ccf824e03216f7a9a934802354e463c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668bd6f7aff476fd432b6198c872efc1ea7a9bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
